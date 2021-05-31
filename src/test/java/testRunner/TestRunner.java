b1cb09d33984480ad483a5084250092402ba51d1
Md Hasibul Hasan Rahat <hasibrahat10@gmail.com>
2021-05-31 14:56:00 +0600

    Tag add for the scenario

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/features"},
-        tags = {""},
+        tags = {"@contact-us"},
         glue = "stepDefinitions",
         monochrome = true,
         strict = true,
--- test-output/report.js
+++ test-output/report.js
@@ -4,110 +4,54 @@ formatter.feature({
   "description": "",
   "keyword": "Feature"
 });
-formatter.scenario({
-  "name": "Features Menu Screen",
+formatter.background({
+  "name": "",
   "description": "",
-  "keyword": "Scenario"
+  "keyword": "Background"
 });
 formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "I click on features menu",
+  "name": "I click on accept cookies button",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepDefinitions.HomePageSteps.iClickOnAcceptCookiesButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Contact Us",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@contact-us"
+    }
+  ]
+});
+formatter.step({
+  "name": "I click on contact us button",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "stepDefinitions.HomePageSteps.i_click_on_features_menu()"
+  "location": "stepDefinitions.HomePageSteps.iClickOnContactUsButton()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "I observed the text \"QUARTO FEATURES\"",
+  "name": "I have submit the form info and close the success screen",
   "keyword": "And "
 });
 formatter.match({
-  "location": "stepDefinitions.HomePageSteps.i_observed_the_text(java.lang.String)"
+  "location": "stepDefinitions.HomePageSteps.iHaveSubmitTheFormInfoAndCloseTheSuccessScreen()"
 });
 formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Industries menu screen visit",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.scenario({
-  "name": "I want to login Quarto with any email",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on sign in link",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "stepDefinitions.HomePageSteps.i_click_on_sign_in_link()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I have entered an email id and click on next",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "stepDefinitions.HomePageSteps.i_have_entered_an_email_id_and_click_on_next()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I get an error alert \"Couldn\u0027t find your quarto account.\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "stepDefinitions.HomePageSteps.i_get_an_error_alert(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Get back to home screen",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "stepDefinitions.HomePageSteps.get_back_to_home_screen()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Quarto Cookies",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on accept cookies",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "stepDefinitions.HomePageSteps.i_click_on_accept_cookies()"
-});
-formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#name\"}\n  (Session info: chrome\u003d90.0.4430.212)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-GQQUAC4\u0027, ip: \u0027192.168.0.183\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002711.0.11\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 90.0.4430.212, chrome: {chromedriverVersion: 90.0.4430.24 (4c6d850f087da..., userDataDir: C:\\Users\\BHALOG~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:50253}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 6429b780c48f3998667f0a758ffeec2a\n*** Element info: {Using\u003did, value\u003dname}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementById(RemoteWebDriver.java:372)\r\n\tat org.openqa.selenium.By$ById.findElement(By.java:188)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy20.clear(Unknown Source)\r\n\tat pages.HomePage.setFormDetails(HomePage.java:151)\r\n\tat stepDefinitions.HomePageSteps.iHaveSubmitTheFormInfoAndCloseTheSuccessScreen(HomePageSteps.java:89)\r\n\tat ✽.I have submit the form info and close the success screen(file:///D:/Testing%20Projects/QuartolabAutomation/src/test/java/features/HomePage.feature:31)\r\n",
+  "status": "failed"
 });
 formatter.after({
   "status": "passed"
